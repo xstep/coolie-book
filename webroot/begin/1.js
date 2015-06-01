afcbc4675fe7c488aaf4e274430b9335ec8383df
张云来 <cloudcome@163.com>
2015-06-01 20:48:58 +0800

    publish

---
+++ webroot/begin/1.js
@@ -0,0 +1,9 @@
+coolie.config({
+    base: "./app/",
+    host: "",
+    debug: !1,
+    version: {
+        "area.js": "355b4d4018bf77cfd42cb6065abceefd",
+        "length.js": "5d113baeb803b345fa0bb6e42c9faaa6"
+    }
+}).use();
