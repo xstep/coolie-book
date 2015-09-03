c3731c2a7b22e096f80991b510f2ac731214bf6e
张云来 <cloudcome@163.com>
2015-09-03 18:40:52 +0800

    coolie book

--- webroot-dev/static/js/app/book.js
+++ webroot-dev/static/js/app/book.js
@@ -6,11 +6,36 @@
 
 
 define(function (require, exports, module) {
-    /**
-     * @module parent/book
-     */
-
     'use strict';
 
     var xhr = require('../alien/core/communication/xhr.js');
+    var selector = require('../alien/core/dom/selector.js');
+    var attribute = require('../alien/core/dom/attribute.js');
+    var $navs = selector.query('#nav li');
+    var app = {};
+    var navMap = {};
+
+    // 实现 nav
+    app.buildNav = function () {
+        var $navLink = selector.query('#nav a');
+
+        $navLink.forEach(function ($link) {
+            var href = $link.href;
+
+            navMap[href] = selector.parent($link);
+        });
+    };
+
+    // 监听 pathname
+    app.listenNav = function () {
+        var pathname = location.pathname;
+        var className = 'active';
+        var $nav = navMap[pathname];
+
+        attribute.removeClass($navs, className);
+        attribute.addClass($nav, className)
+    };
+
+    app.buildNav();
+    app.listenNav();
 });
