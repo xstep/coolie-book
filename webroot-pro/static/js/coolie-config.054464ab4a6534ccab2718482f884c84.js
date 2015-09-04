4b530f1924bbce166b066c7c155077fe252f5ebf
张云来 <cloudcome@163.com>
2015-09-04 17:55:19 +0800

    release

--- webroot-pro/static/js/coolie-config.054464ab4a6534ccab2718482f884c84.js
+++
@@ -1,2 +0,0 @@
-/*coolie@0.22.8*/
-coolie.config({base:"./app/",debug:!1,cache:!0,version:{"book.js":"7124befc1950a5947c013ea44af4a712"}}).use();
---
+++ webroot-pro/static/js/coolie-config.a1b7552ca0a1b8ad7562d62da6cc73af.js
@@ -0,0 +1,2 @@
+/*coolie@0.22.8*/
+coolie.config({base:"./app/",debug:!1,cache:!0,version:{"book.js":"ebfca4461cd2f37adb8979b5c7e378fd"}}).use();
